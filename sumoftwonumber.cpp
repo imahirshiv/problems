2ecf6dd6a3371c2865b847cdbda7cda5c154c9e4
Ahir Shiv <imahirshiv@gmail.com>
2022-12-11 15:14:04 +0530

    SomeExamples

---
+++ sumoftwonumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a, b, sum;
+    cin >> a;
+    cout << "Entered number a is :" << a << endl;
+    cin >> b;
+    cout << "Entered Number b is :" << b << endl;
+    sum = a + b;
+    cout << "Sum of entered number is: " << sum << endl;
+
+    return 0;
+}
