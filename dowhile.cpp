e348392ba8dee088fc1782109a3afe46fe482880
Ahir Shiv <imahirshiv@gmail.com>
2022-12-16 11:39:21 +0530

    intialc miit

---
+++ dowhile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a =1;
+    do
+    {   
+        cout<<a<<" This is my best time ! god thankYOu!"<<endl;
+        a++;
+    } while (a<=100);
+    
+
+    return 0;
+}
