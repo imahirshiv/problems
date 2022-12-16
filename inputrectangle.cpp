50bbe8dd9b4008781353f8e4360274370bad4357
Ahir Shiv <imahirshiv@gmail.com>
2022-12-16 18:44:12 +0530

    initil commit

--- inputrectangle.cpp
+++ inputrectangle.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-    int starRow,starColumn;
-    cout<<"Enter star Row :";
-    cin>>starRow;
-    cout<<"Enter star Column :";
-    cin>>starColumn;
+    int starRow, starColumn;
+    cout << "Enter star Row :";
+    cin >> starRow;
+    cout << "Enter star Column :";
+    cin >> starColumn;
 
     for (int line = 1; line <= starRow; line++)
     {
         for (int i = 1; i <= starColumn; i++)
         {
-            cout<<"*";
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
 
     return 0;
 }
