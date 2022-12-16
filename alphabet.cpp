0c7528fdbf18df80b226a3c721a99e22627c59b4
Ahir Shiv <imahirshiv@gmail.com>
2022-12-16 14:02:16 +0530

    intialc oo

--- alphabet.cpp
+++ alphabet.cpp
@@ -7,14 +7,19 @@ int main()
     cout << "Enter n:- ";
     cin >> n;
 
+    // For Upper traiangle
+
     for (int line = 1; line <= n; line++)
     {
+        // For Spaces
 
         int space = (n - line);
         for (int k = 0; k < space; k++)
         {
             cout << " ";
         }
+
+        // For character
 
         int no_of_char = 2 * line - 1;
 
@@ -25,13 +30,22 @@ int main()
         }
         cout << endl;
     }
+
+    // For Lower triangle
+
     for (int line = n + 1; line <= 2 * n - 1; line++)
     {
+
+        // For Spaces
+
         int numberOfSpace = (line - n);
         for (int k = 0; k < numberOfSpace; k++)
         {
             cout << " ";
         }
+
+        // For Characters
+
         int numberOfChar = 2 * (2 * n - line) - 1;
         for (int j = 0; j < numberOfChar; j++)
         {
