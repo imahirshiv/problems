3dd58442283ea129c4bcf387b75b2576caf64f5f
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 19:41:05 +0530

    intial commit

---
+++ someMorePatternProblem/greeting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+string greetings(string x)
+{
+
+    x = "Wellcoming You Sir!!";
+    return x;
+}
+
+int main()
+{
+
+        string n="Hey!!";
+    cout<<n<<" My name is Rahul and i am "<<greetings(n)<<endl;
+
+    return 0;
+}
