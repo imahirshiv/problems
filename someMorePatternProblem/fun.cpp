2cd0f785e606cc1fd4976525b67ee4e60ed1d722
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 22:24:40 +0530

    intial commit

---
+++ someMorePatternProblem/fun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void fun(string name)
+{
+    cout << "Are You having funn!!" << name << "?!" << endl;
+}
+int main()
+{
+
+    fun("Priyanka");
+    fun("Rani?");
+    fun("Shiv");
+
+    return 0;
+}
