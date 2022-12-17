8a746c7255efcee37ad7618eed54d4135be0a977
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 22:49:15 +0530

    intial commit

--- someMorePatternProblem/aditon.cpp
+++ someMorePatternProblem/aditon.cpp
@@ -1,17 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int additon(int a, int b){
-    int sum=a+b;
+int additon(int a, int b)
+{
+    int sum = a + b;
     return sum;
 }
-int main(){
+void fun(string name)
+{
+    cout << "Are You having fun?!!1" << name << "?!" << endl;
+}
+int main()
+{
 
-    int a,b;
-    cout<<"Enter a: ";
-    cin>>a;
-    cout<<"Entet b: ";
-    cin>>b;
-    cout<<additon(a,b)<<endl;
-
+    int a, b;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Entet b: ";
+    cin >> b;
+    cout << additon(a, b) << endl;
+    fun("Ahir Shiv");
     return 0;
 }
