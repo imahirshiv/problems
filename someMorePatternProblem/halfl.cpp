b813d259b38112fe50275529900e6f009e90f151
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 19:19:14 +0530

    intial commit

--- someMorePatternProblem/halfl.cpp
+++ someMorePatternProblem/halfl.cpp
@@ -1,21 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-
-    int n;
-    cout<<"Enter n: ";
-    cin>>n;
+void halfpyramid(int n)
+{
 
     for (int i = n; i >= 1; i--)
     {
-        for (int j = 1; j <=i ; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout<<"*";
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
-    cout<<endl;
 
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    cout<<endl;
+    halfpyramid(n);
+    halfpyramid(n);
+    halfpyramid(n);
+    halfpyramid(n);
+    halfpyramid(n);
+    halfpyramid(n);
     return 0;
 }
