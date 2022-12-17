d5c7aaa96e163da58df46db1b08e3f4a15b17876
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 23:25:20 +0530

    initial commit

---
+++ naturals.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int sqr(int x)
+{
+    int sq = x * x;
+    return sq;
+}
+int main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << sqr(i) << endl;
+    }
+
+    return 0;
+}
