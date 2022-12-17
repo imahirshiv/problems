b9c4e93e5d3fcdee8757d46cc8aa1885456e754d
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 09:01:21 +0530

    intail ocmmit

--- someMorePatternProblem/halftranigle.cpp
+++ someMorePatternProblem/halftranigle.cpp
@@ -5,9 +5,9 @@ int main()
     int n;
     cout << "Enter n: ";
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 1; j <= i; j++)
         {
             cout << "*";
         }
