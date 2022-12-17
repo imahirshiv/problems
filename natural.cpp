3e1c03f3985f75ebbe98ddb4547be734bacb97c2
Ahir Shiv <imahirshiv@gmail.com>
2022-12-17 23:21:42 +0530

    intiail commit

---
+++ natural.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+// int sqr(int);
+// int i=1;
+// int x=i*i;
+
+int main(){
+    for (int i = 1; i <= 5; i++)
+    {
+        cout<<pow(i,2)<<endl;
+        
+    }
+    
+
+// int sqr();
+
+    return 0;
+}
+// int sqr(int){
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         int x= i*i;
+//         cout<<x<<endl;
+//     }
+//     return x;
+// }
