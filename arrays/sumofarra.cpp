3ff2b35b074bcb05e1443a1d55958e8a17444e4a
Ahir Shiv <imahirshiv@gmail.com>
2022-12-18 14:59:51 +0530

    intial commit

---
+++ arrays/sumofarra.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int array[]={3,4,5,34,22};
+    int size=sizeof(array)/sizeof(array[0]);
+
+    int sum=0;
+    for (int i = 0; i < size; i++)
+    {
+        sum+=array[i];
+    }
+    cout<<sum;
+   cout<<endl;
+    return 0;
+}
