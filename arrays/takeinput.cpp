85ac5dd5ca2c70a80d4e60e2cc7335ad6755318f
Ahir Shiv <imahirshiv@gmail.com>
2022-12-18 14:29:49 +0530

    intial commit

---
+++ arrays/takeinput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int array[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>array[i];
+    }
+    cout<<endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<array[i]<<endl;
+    }
+
+    return 0;
+}
---
+++ arrays/vovels.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    char vowels[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>vowels[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<vowels[i]<<" ";
+    }
+    return 0;
+}
