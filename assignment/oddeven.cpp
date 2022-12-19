9122a5413cd593f7b612d246d7d9b94da0933f1b
Ahir Shiv <imahirshiv@gmail.com>
2022-12-19 09:48:07 +0530

    intial commit

---
+++ assignment/oddeven.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int count(int arr[], int n)
+{
+    int count_odd = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            count_odd++;
+        }
+    }
+    return count_odd;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter Size of array: ";
+    cin >> n;
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    int odd = count(array, n);
+    cout << "The odd count is : " << odd << endl;
+    cout << "The even coutn is : " << n - odd << endl;
+
+    return 0;
+}
