267fbdfdb77a22b62123d413a222e40c3e14b477
Ahir Shiv <imahirshiv@gmail.com>
2022-12-20 18:05:53 +0530

    intal commit

---
+++ assignment/rev.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void rev(int array[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start < end)
+    {
+        int temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
+        start++;
+        end--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << "";
+    }
+   
+}
+
+int main()
+{
+    int n=5;
+    int array[]={5,6,7,8,9};
+    
+    cout << "The reverse array is :" << rev(array, n) << endl;
+    return 0;
+}
