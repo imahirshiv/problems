4ec2b718e6c57f29bb3aa1590aa60e87ecef083c
Ahir Shiv <imahirshiv@gmail.com>
2022-12-22 17:36:45 +0530

    intal commit

--- vector/vec.cpp
+++ vector/vec.cpp
@@ -6,10 +6,14 @@ using namespace std;
 int main(){
    
     vector<int> vec(10);
-    cout<<vec.size();
+    cout<<"The capacity is : "<<vec.capacity()<<endl;
+    cout<<"The size of first:"<<vec.size();
     cout<<endl;
     vec.resize(3);
+    cout<<"New capacity "<<vec.capacity()<<endl;
     cout<<vec.size();
     cout<<endl;
+    vec.push_back(1);
+        
     return 0;
 }
